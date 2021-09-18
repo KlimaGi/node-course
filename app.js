2cbe65a8c2deca40e3e157399442be4fe634be7a
Giedre Klimaite <giedre.klima@gmail.com>
2021-09-18 13:10:40 +0300

    printing in collors

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const validator = require("validator");
+const chalk = require("chalk");
+
 const getNotes = require("./notes.js");
 const msg = getNotes();
 console.log(msg);
 
+console.log(chalk.green("Success!"));
+console.log(chalk.bold("Success!"));
+console.log(chalk.green.inverse("Success!"));
+console.log(chalk.bgCyan("Success!"));
 //console.log(validator.isEmail("er.com"));
-console.log(validator.isURL("https/mead.io"));
+//console.log(validator.isURL("https/mead.io"));
